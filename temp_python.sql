38ad61c5557de7d20ed014d0f7ec7947b293421c
leocareer <leo.ph.career@gmail.com>
2024-06-27 00:36:46 +0200

    modified 180624_exam.sql, deleted: temp_python.sql

--- temp_python.sql
+++
@@ -1,9 +0,0 @@
-select count(c.medal_id) medals, d.age
-from olympics.competitor_event c
-inner join olympics.games_competitor d
-on c.competitor_id = d.id
-where c.medal_id in (1, 2, 3)
-group by d.age;
-
-select world.country.SurfaceArea area, world.country.population 
-from world.country;
